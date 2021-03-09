47f18aaddf88d4f14c5eb539c85c5aae922acbcc
Daniel Iovescu <iovescu.dani@gmail.com>
2021-03-10 00:40:40 +0200

    solving of the exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ManualComponent } from './components/manual/manual.component';
+import { GeneratedComponent } from './components/generated/generated.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManualComponent,
+    GeneratedComponent
   ],
   imports: [
     BrowserModule
